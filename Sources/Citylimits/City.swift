9f065e95f582230ae78ad6f0e1d4c042f6f10fbd
Tony Bark <35226681+tonytins@users.noreply.github.com>
2026-08-12 09:25:52 -0400

    implemented Updatable and Resettable

--- Sources/Citylimits/City.swift
+++ Sources/Citylimits/City.swift
@@ -21,13 +21,46 @@ protocol Updatable {
     mutating func update(dt: DeltaTime)
 }
 
-public struct City {
-    
+public struct City: Updatable, Resettable {
+   
     public static let bulldozeCost = 10
     public static let randomDisasterChance = 0.02
     public static let trainStationTaxRevenue = 50
     
     public static let minTreasury = Int.min / 2
     public static let maxTreasury = Int.max / 2
+    
+    var storedTreasury: Int
+    
+    public let startingFunds: Int
+    public var population: Int
+    public var taxIncomeLastTick: Int
+    public var maintenanceLastTick: Int
+    
+    var accumulatedTime: DeltaTime
+    var rng: any RandomNumberGenerator
+    
+    public var treasurey: Int {
+        get { storedTreasury }
+        set { storedTreasury = min(max(newValue, City.minTreasury), City.maxTreasury) }
+    }
+    
+    mutating func update(dt: DeltaTime) {
+        accumulatedTime += dt
+        let tickInterval: DeltaTime = 1.0
+        
+        while accumulatedTime >= tickInterval {
+            accumulatedTime -= tickInterval
+            // TODO: the entire Micropolis game loop xD
+        }
+    }
+    
+    mutating func reset() {
+        // TODO: clear map and any disaster
+        treasurey = startingFunds
+        population = 0
+        taxIncomeLastTick = 0
+        maintenanceLastTick = 0
+    }
     
 }
